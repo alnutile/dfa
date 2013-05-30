b96479e2cdd88cf1182983ec2a171d035ee7f872
Alfred Nutile <al@freshjones.com>
2013-05-30 04:52:38 -0400

    Admin interface now works. Going to add those other fields and tests

--- app/admin/speakers.rb
+++ app/admin/speakers.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Speaker do
-  
+
 end
--- app/controllers/speaker_requests_controller.rb
+++ app/controllers/speaker_requests_controller.rb
@@ -1,2 +1,7 @@
 class SpeakerRequestsController < InheritedResources::Base
+
+  def index
+    @speaker_requests = SpeakerRequest.paginate(page: params[:page], :per_page => 20)
+  end
+
 end
