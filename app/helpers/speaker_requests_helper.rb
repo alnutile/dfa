24ce79fa007c05e9b24808b706377064929d5665
Alfred Nutile <al@freshjones.com>
2013-05-29 20:30:44 -0400

    Refactor so the helper is there

--- app/helpers/speaker_requests_helper.rb
+++ app/helpers/speaker_requests_helper.rb
@@ -1,2 +1,20 @@
 module SpeakerRequestsHelper
+
+	def speakerName(speakerRequestObject) 
+		 if speakerRequestObject.speaker_id
+		 	@speakerName = speakerRequestObject.speaker.firstname
+		 else 
+		 	@speakerName = "<em>[no speaker click here to sign up]</em>"
+		 end
+	end
+
+
+	def locationName(speakerRequestObject) 
+		 if speakerRequestObject.location_id
+		 	@locationName = speakerRequestObject.location.name
+		 else 
+		 	@locationName = "<em>[no location yet]</em>"
+		 end
+	end
+
 end
