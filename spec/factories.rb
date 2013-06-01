eeb6384cf34f2e0a530e2deda3d710dd974b3930
Alfred Nutile <al@freshjones.com>
2013-05-31 22:00:46 -0400

    clean up example tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,5 +7,15 @@ FactoryGirl.define do
     sequence(:firstname)  { |n| "Speaker #{n}" }
   end
 
-end
+  factory :speaker_request do
+    sequence(:title)  { |n| "Speaker #{n}" }
+    sequence(:content) { |n| "Coming Soon..." }   
+    sequence(:speaker_id) { |n| n }   
+    sequence(:location_id) { |n| n }
+    sequence(:date) { |n| Time.now }
+    sequence(:train_trainer) { |n| 1 }
+    sequence(:cme_ceu) { |n| 1 }
+    sequence(:public) { |n| 1 }
 
+  end
+end
--- spec/requests/speaker_requests_spec.rb
+++ spec/requests/speaker_requests_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe "SpeakerRequests" do
-  describe "GET /speaker_requests" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get speaker_requests_path
-      response.status.should be(200)
-    end
-  end
+ 	subject { page }
+
+ 	describe "index" do
+ 		let(:speaker_request) { FactoryGirl.create(:speaker_request) }
+ 		let(:location) { FactoryGirl.create(:location) }
+ 		let(:speaker) { FactoryGirl.create(:speaker) }
+
+ 		before(:all) do 
+			10.times { FactoryGirl.create(:location) } 
+			10.times { FactoryGirl.create(:speaker) } 
+			10.times { FactoryGirl.create(:speaker_request) } 
+		end
+
+ 		after(:all) { Speaker_Request.delete_all }
+
+ 		before(:each) do
+ 			visit speaker_requests_path
+ 		end
+
+ 		it { should have_selector('h3', text: 'Speaker Requests') }
+ 
+ 	end
 end
