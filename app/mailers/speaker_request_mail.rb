8fe2c97f69942eaa8ddcd2b3997cd59bdff97dd2
Alfred Nutile <alfrednutile@gmail.com>
2013-06-08 08:09:32 -0400

    Mailer work

--- app/mailers/speaker_request_mail.rb
+++ app/mailers/speaker_request_mail.rb
@@ -5,6 +5,7 @@ class SpeakerRequestMail < ActionMailer::Base
 
   def new_request_notify(speaker_request)
     #@id = request.id
+    @url = "http://dfa.alfrednutile.info"
     @speaker_request = speaker_request
     mail(to: "alfrednutile@gmail.com", subject: "New Request")
     #logger.info "Email Sent #{@speaker_request}"
