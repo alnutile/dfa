94bd7f0f45da1ccac214171047711940ce0a2d1e
Alfred Nutile <alfrednutile@gmail.com>
2013-06-12 16:20:53 -0400

    Final test for now

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,13 +4,15 @@ FactoryGirl.define do
   end
 
   factory :speaker_request do
+    date_now = Time.now + 1.day
     sequence(:speaker_id) { |n| n } 
-    sequence(:date) { |n| Time.now + 1.day } 
+    sequence(:date) { |n| date_now } 
     #sequence(:tag_list) { |n| "Tag #{n}" }
     #sequence(:city_list) { |n| "City #{n}" }
     #sequence(:state) { |n| "State #{n}" }
     #sequence(:location_street) { |n| "Street #{n}" }
     #public 1
+    published 1
     content 'Coming Soon...' 
     cme_ceu 1
     train_trainer 1
--- spec/requests/speaker_requests_spec.rb
+++ spec/requests/speaker_requests_spec.rb
@@ -4,29 +4,30 @@ describe "SP Search page" do
 
   subject { page }
 
-#describe "Results" do
-#    let(:speaker_request) { FactoryGirl.create(:speaker_request) }
-#    let(:speaker) { FactoryGirl.create(:speaker) }
-#    
-#    before(:each) do
-#      visit speaker_requests_path
-#    end
-#
-#    it "should have a view link" do
-#      should have_selector('h3', text: 'Speaker Requests')
-#    end
-#
-#    describe "pagination" do
-#      before(:all) do
-#        31.times { FactoryGirl.create(:speaker) }
-#        31.times { FactoryGirl.create(:speaker_request) }
-#      end
-#      
-#      it { should have_selector('div.pagination') }
-#    end
-#
-#  end
-#
+ describe "Results" do
+     let(:speaker_request) { FactoryGirl.create(:speaker_request) }
+     let(:speaker) { FactoryGirl.create(:speaker) }
+     
+     before(:each) do
+       visit speaker_requests_path
+     end
+ 
+     it "should have a view link" do
+       should have_selector('h3', text: 'Speaker Requests')
+     end
+ 
+     describe "pagination" do
+       before(:each) do
+         31.times { FactoryGirl.create(:speaker) }
+         31.times { FactoryGirl.create(:speaker_request) }
+         visit '/requests'
+       end
+       it { should have_content('Speaker 1') }       
+       it { should have_content('Time') }
+     end
+ 
+   end
+ 
   describe "Submit Request" do
     before do
       visit speaker_requests_path
