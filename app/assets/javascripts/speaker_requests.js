eb6b3cde64a69a316cd062675fd864f013710fa1
Alfred Nutile <alfrednutile@gmail.com>
2013-06-21 21:44:07 -0400

    fix calendar filters

--- app/assets/javascripts/speaker_requests.js
+++ app/assets/javascripts/speaker_requests.js
@@ -10,21 +10,25 @@ $('document').ready(function() {
    var search = '';
    if(window.location.search.length) {
      search = window.location.search.substring(1);
-     search = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-   }
+     search = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'\":\"') + '"}');
+   };
 
-   console.log(search);
    $('select#state').on('change', function() {
      $("select#state option:selected").each(function(){
        var selected = $(this).text();
-       var loca = window.location.href;
+       var loca = window.location.origin;
+
        if( search.hasOwnProperty('date') ) {
+          query = window.location.search;
           params = '&state='+selected;
+          window.location.href = loca + '/calendar' + query + params;
+       } else if( search.hasOwnProperty('state') ) {
+          params = '?state='+selected;
+          window.location.href = loca + '/calendar' + params;
        } else {
           params = '?state='+selected;
+          window.location.href = loca + '/calendar' + params;
        }
-       console.log(loca + '?state='+selected);
-       window.location.href = loca + params;
      });
    });
 
