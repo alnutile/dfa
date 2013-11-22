36b4013c161c3cff349ae3f54aea6b6d7628eed1
basurveycom@gmail.com <basurveycom@gmail.com>
2013-11-21 21:22:33 -0500

    Precompile off for heroku

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,8 @@ module Drfa
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
